fab6590c570fa7a38022625a986dc0ccc17ca98b
Bojan Zivanovic <bojanz@gmail.com>
2020-10-30 00:55:21 +0100

    Shorten doc.go, the address struct should not be shown twice.

--- doc.go
+++ doc.go
@@ -11,39 +11,9 @@
 //   5. HTML formatter.
 //   6. HTTP handler for serving address formats and regions as JSON: only ~14kb gzipped!
 //
-// Address struct
-//
-// Represents an address as commonly handled by web applications and APIs.
-//
-//   type Address struct {
-//      Line1 string
-//      Line2 string
-//      Line3 string
-//      // Sublocality is the neighborhood/suburb/district.
-//      Sublocality string
-//      // Locality is the city/village/post town.
-//      Locality string
-//      // Region is the state/province/prefecture.
-//      // An ISO code is used when available.
-//      Region string
-//      // PostalCode is the postal/zip/pin code.
-//      PostalCode string
-//      // CountryCode is the two-letter code as defined by CLDR.
-//      CountryCode string
-//   }
-//
-// Recipient fields such as FirstName/LastName are not included since they are usually
-// present on the parent (Contact/Customer/User) struct. This allows the package to
-// avoid being opinionated about name handling.
-//
-// There are three line fields, matching the HTML5 autocomplete spec and many shipping APIs.
-// This leaves enough space for specifying an organization and department, house or hotel
-// name, and other similar "care of" use cases. When mapping to an API that only has two address
-// lines, Line3 can be appended to Line2, separated by a comma.
-//
 // Address formats
 //
-// The following information is available:
+// The following address format information is available for each country:
 //   - Which fields are used, and in which order.
 //   - Which fields are required.
 //   - Labels for the sublocality, locality, region and postal code fields.
