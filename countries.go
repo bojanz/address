67648f745e5cab365a5869c972a528cfc2a8b40c
Bojan Zivanovic <bojan.zivanovic@platform.sh>
2025-12-25 11:09:41 +0100

    Update to CLDR v48. No data changes.

--- countries.go
+++ countries.go
@@ -4,7 +4,7 @@
 package address
 
 // CLDRVersion is the CLDR version from which the country list is derived.
-const CLDRVersion = "47.0.0"
+const CLDRVersion = "48.0.0"
 
 var countries = map[string]string{
 	"AF": "Afghanistan",
